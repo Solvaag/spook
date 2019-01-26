4004b38c965f6ca94161e9fc73ab1123e41ee7e0
StationarySolvaag <sigve.solvaag@gmail.com>
2019-01-26 23:54:27 +0100

    Added a SpriteSheet class.
    Tested SpriteSheet class successfully.

--- classes/render/images.js
+++ classes/render/images.js
@@ -37,3 +37,49 @@ class ImageManager {
         }
     }
 }
+
+class SpriteSheet {
+
+    constructor(image, columns, tile_height, tile_width) {
+        this.image = image;
+        this.columns = columns;
+        this.rows = (image.height / tile_height);
+        this.tile_height = tile_height;
+        this.tile_width = tile_width;
+        this.images = this.split_image();
+    }
+
+    split_image() {
+        let sequence = [];
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.columns; col++) {
+                let sx = col * this.tile_width;
+                let sy = row * this.tile_height;
+                let s_width = this.tile_width;
+                let s_height = this.tile_height;
+                const sprite = new SpriteElement(sx, sy, s_width, s_height);
+                sequence.push(sprite);
+            }
+        }
+        return sequence;
+    }
+
+    draw(sprite_id, position, canvas) {
+        let sprite = this.images[sprite_id];
+
+        let delta_width = this.tile_width;
+        let delta_height = this.tile_height;
+        canvas.drawImage(this.image, sprite.sx, sprite.sy, sprite.s_width, sprite.s_height, position.x, position.y, delta_width, delta_height);
+
+    }
+
+}
+
+class SpriteElement {
+    constructor (sx, sy, s_width, s_height){
+        this.sx = sx;
+        this.sy = sy;
+        this.s_width = s_width;
+        this.s_height = s_height;
+    }
+}
